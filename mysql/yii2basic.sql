ebca8898b015f70e3843525654e6776eab18d81c
Besteven <stefanostei18@gmail.com>
2022-09-28 07:10:11 +0300

    Add mysql folder

---
+++ mysql/yii2basic.sql
@@ -0,0 +1,132 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Sep 26, 2022 at 02:25 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `yii2basic`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `areas`
+--
+
+CREATE TABLE `Areas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `notes` text DEFAULT NULL,
+  `state` int(2) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `billing_type`
+--
+
+CREATE TABLE `Billing_type` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `area_id` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `notes` text DEFAULT NULL,
+  `state` int(11) NOT NULL DEFAULT 0,
+  `price` float DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `billing_type_ibfk_1` FOREIGN KEY (`area_id`) REFERENCES `areas` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customers`
+--
+
+CREATE TABLE `Customers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `fname` varchar(255) DEFAULT NULL,
+  `lname` varchar(255) DEFAULT NULL,
+  `afm` varchar(20) DEFAULT NULL,
+  `doy` varchar(20) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `phone` varchar(15) DEFAULT NULL,
+  `at` varchar(10) DEFAULT NULL,
+  `protocol_no` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `earth_type`
+--
+
+CREATE TABLE `Earth_type` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `area_id` int(11) NOT NULL,
+  `billing_type_id` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `notes` text NOT NULL,
+  `billing_type` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `earth_type_ibfk_1` FOREIGN KEY (`area_id`) REFERENCES `areas` (`id`),
+  CONSTRAINT `earth_type_ibfk_2` FOREIGN KEY (`billing_type_id`) REFERENCES `billing_type` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `estates`
+--
+
+CREATE TABLE `Estates` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `customer_id` int(11) DEFAULT NULL,
+  `type` varchar(50) DEFAULT NULL,
+  `size` float DEFAULT NULL,
+  `area_id` int(11) DEFAULT NULL,
+  `notes` text DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `estates_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`id`),
+  CONSTRAINT `estates_ibfk_2` FOREIGN KEY (`area_id`) REFERENCES `areas` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payments`
+--
+
+CREATE TABLE `Payments` (
+  `payment_no` int(11) NOT NULL AUTO_INCREMENT,
+  `customer_id` int(11) DEFAULT NULL,
+  `price` float DEFAULT NULL,
+  `billing_id` int(11) DEFAULT NULL,
+  `type` varchar(20) DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  PRIMARY KEY (`payment_no`),
+  CONSTRAINT `payments_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`id`),
+  CONSTRAINT `payments_ibfk_2` FOREIGN KEY (`billing_id`) REFERENCES `billing_type` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
